63b467280f47f46323fab2b085e948edb7dfbe20
Cloudbed_invi <sriharshakota2004@gmail.com>
2026-07-14 22:38:12 +0530

    Reduce scroll wheel sensitivity to 0.05

--- src/components/MapGrid.jsx
+++ src/components/MapGrid.jsx
@@ -400,7 +400,7 @@ export function MapGrid({ cellColors, activeColor, onCellPointerDown, onCellPoin
         }}
         pinch={{ disabled: false }}
         doubleClick={{ disabled: false, mode: 'reset' }}
-        wheel={{ disabled: false }}
+        wheel={{ disabled: false, step: 0.05 }}
         style={{ width: "100%", height: "100%" }}
       >
         <TransformComponent wrapperStyle={{ width: "100%", height: "100%" }} contentStyle={{ width: "100%", height: "100%" }}>
